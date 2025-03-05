a9bd5449151b09fa00559b6e4fcfb9fb3f8b983a
Logic-Koike <koike@logic-biz.co.jp>
2025-03-05 11:11:46 +0900

    クラスタ表示OK

---
+++ lib/core/maptest/marker_widget.dart
@@ -0,0 +1,71 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class TestMarkers {
+  static const osakaStation = LatLng(34.702485, 135.495951);
+
+  static List<Marker> getTestMakers() {
+    var markers = <Marker>[];
+
+    markers.add(Marker(
+      point: osakaStation,
+      child: const Icon(Icons.pin_drop),
+    ));
+
+    final totalMarkers = 1000;
+
+    final minLatitude = 34.5;
+    final maxLatitude = 35.0;
+    final minLongitude = 135.0;
+    final maxLongitude = 135.6;
+
+    for (var i = 0; i < totalMarkers; i++) {
+      final latitude =
+          minLatitude + Random().nextDouble() * (maxLatitude - minLatitude);
+      final longitude =
+          minLongitude + Random().nextDouble() * (maxLongitude - minLongitude);
+
+      markers.add(Marker(
+        point: LatLng(latitude, longitude),
+        child: const Icon(
+          Icons.pin_drop,
+          color: Colors.orange,
+        ),
+      ));
+    }
+
+    // final totalMarkers = 2000.0;
+    // final minLatLng = const LatLng(49.8566, 1.3522);
+    // final maxLatLng = const LatLng(58.3498, -10.2603);
+
+    // final latitudeRange = maxLatLng.latitude - minLatLng.latitude;
+    // final longitudeRange = maxLatLng.longitude - minLatLng.longitude;
+
+    // final stepsInEachDirection = sqrt(totalMarkers).floor();
+    // final latStep = latitudeRange / stepsInEachDirection;
+    // final lonStep = longitudeRange / stepsInEachDirection;
+
+    // for (var i = 0; i < stepsInEachDirection; i++) {
+    //   for (var j = 0; j < stepsInEachDirection; j++) {
+    //     final latLng = LatLng(
+    //       minLatLng.latitude + i * latStep,
+    //       minLatLng.longitude + j * lonStep,
+    //     );
+
+    //     markers.add(
+    //       Marker(
+    //         height: 30,
+    //         width: 30,
+    //         point: latLng,
+    //         child: const Icon(Icons.pin_drop),
+    //       ),
+    //     );
+    //   }
+    // }
+
+    return markers;
+  }
+}
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:quickstart_firebase/core/maptest/marker_widget.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -11,6 +13,13 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   final initialPos = LatLng(34.702485, 135.495951);
+  late final List<Marker> markers;
+
+  @override
+  void initState() {
+    markers = TestMarkers.getTestMakers();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,44 +38,69 @@ class _MapPageState extends State<MapPage> {
                   "https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png",
               userAgentPackageName:
                   'com.logic.exampleapp', // Add your app identifier
-
-              // And many more recommended properties!
             ),
             RichAttributionWidget(attributions: [
               TextSourceAttribution(
                 "国土地理院\nhttps://maps.gsi.go.jp/development/ichiran.html",
               )
             ]),
-            // SimpleAttributionWidget(
-            //   source: Text('マップ表示テスト'),
-            // ),
-            MarkerLayer(
-              markers: [
-                Marker(
-                    point: initialPos,
-                    child: GestureDetector(
-                      child: Icon(
-                        Icons.gps_fixed,
-                        size: 60,
-                        color: Colors.blue,
+            MarkerClusterLayerWidget(
+              options: MarkerClusterLayerOptions(
+                  maxClusterRadius: 45,
+                  size: const Size(40, 40),
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(50),
+                  maxZoom: 15,
+                  markers: markers,
+                  onMarkerTap: (marker) {
+                    final snackBar = SnackBar(content: Text('Tap $initialPos'));
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  },
+                  builder: (context, markers) {
+                    return Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.blue),
+                      child: Center(
+                        child: Text(
+                          markers.length.toString(),
+                          style: const TextStyle(color: Colors.white),
+                        ),
                       ),
-                      onTap: () {
-                        final snackBar =
-                            SnackBar(content: Text('Tap $initialPos'));
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                      },
-                    ),
-                    alignment: Alignment.topLeft),
-                Marker(
-                  point: initialPos,
-                  child: Icon(
-                    Icons.circle,
-                    size: 1,
-                    color: Colors.red,
-                  ),
-                ),
-              ],
-            ),
+                    );
+                  }),
+            )
+            // MarkerLayer(
+            //   markers: [
+            //     Marker(
+            //       point: initialPos,
+            //       width: double.infinity,
+            //       height: double.infinity,
+            //       child: GestureDetector(
+            //         child: Center(
+            //           child: Icon(
+            //             Icons.location_on,
+            //             color: Colors.blue,
+            //             size: 40.0,
+            //           ),
+            //         ),
+            //         onTap: () {
+            //           final snackBar =
+            //               SnackBar(content: Text('Tap $initialPos'));
+            //           ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            //         },
+            //       ),
+            //     ),
+            //     Marker(
+            //       point: initialPos,
+            //       child: Icon(
+            //         Icons.circle,
+            //         size: 1,
+            //         color: Colors.red,
+            //       ),
+            //     ),
+            //   ],
+            // ),
           ]),
     );
   }
